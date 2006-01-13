acc4891ac6145d6e2cc86015b804c05160220335
kohsuke <null>
2006-01-13 21:21:11 +0000

    added goal

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xe
 cd args4j
-maven javanet:dist javanet:site
+maven javanet:dist javanet:site javanet:deploy-jar jar:install
 cd ../args4j-tools
-maven javanet:dist
+maven javanet:dist javanet:deploy-jar jar:install
 cd ..
