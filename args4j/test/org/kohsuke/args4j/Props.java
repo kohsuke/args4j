2234fd48074446c808a2ab06c7828ef1ab0bc0e2
jhm <null>
2006-02-05 18:26:27 +0000

    How to handle properties? Here some test cases for discussion from the users point of view.

---
+++ args4j/test/org/kohsuke/args4j/Props.java
@@ -0,0 +1,17 @@
+package org.kohsuke.args4j;
+
+import java.util.Map;
+
+/**
+ * Test object for setting name-value pairs.
+ * <i>Not implemented in the parser yet. Needs discussion with Kohsuke</i> 
+ * 
+ * @author Jan Matèrne
+ */
+public class Props {
+
+    //TODO implement parsing properties
+    //@Property(prefix="-T",usage="sets a key-value-pair")
+    public Map props;
+
+}
---
+++ args4j/test/org/kohsuke/args4j/PropsTest.java
@@ -0,0 +1,70 @@
+package org.kohsuke.args4j;
+
+import java.util.Map;
+
+public class PropsTest extends Args4JTestBase {
+
+    @Override
+    public Object getTestObject() {
+        return new Props();
+    }
+    
+    public void testDoNothing() {
+    	// ignore, that the other test cases are commented out
+    	// JUnit doesnt like TestCases without a test-method.
+    }
+
+    public void _testNoValues() {
+        args = new String[]{};
+        try {
+            parser.parseArgument(args);
+            Map map = ((Props)testObject).props;
+            assertTrue("Values illegally arrived.", map.size()==0);
+        } catch (CmdLineException e) {
+            fail("Call without parameters is valid!");
+        }
+    }
+    
+    public void _testSinglePair() {
+        args = new String[]{"-Tkey1=value1"};
+        try {
+            parser.parseArgument(args);
+            Map map = ((Props)testObject).props;
+            assertTrue("The key was not set.", map.containsKey("key1"));
+            assertEquals("More keys than expected.", map.size(), 1);
+            assertEquals("Key has wrong value", map.get("key1"), "value1");
+        } catch (CmdLineException e) {
+            fail("Cought an invalid exception");
+        }
+    }
+    
+    public void _testMultiplePairs() {
+        args = new String[]{"-Tkey1=value1", "-Tkey2=value2", "-Tkey3=value3"};
+        try {
+            parser.parseArgument(args);
+            Map map = ((Props)testObject).props;
+            assertTrue("A key was not set.", map.containsKey("key1"));
+            assertTrue("A key was not set.", map.containsKey("key2"));
+            assertTrue("A key was not set.", map.containsKey("key3"));
+            assertEquals("Wrong number of keys.", map.size(), 3);
+            assertEquals("Key has wrong value", map.get("key1"), "value1");
+            assertEquals("Key has wrong value", map.get("key2"), "value2");
+            assertEquals("Key has wrong value", map.get("key3"), "value3");
+        } catch (CmdLineException e) {
+            fail("Cought an invalid exception");
+        }
+    }
+
+    public void _testDuplicateKey() {
+        args = new String[]{"-Tkey1=value1", "-Tkey1=value1"};
+        try {
+            parser.parseArgument(args);
+            Map map = ((Props)testObject).props;
+            assertTrue("A key was not set.", map.containsKey("key1"));
+            assertEquals("Wrong number of keys.", map.size(), 1);
+            // TODO: which value is set?
+        } catch (CmdLineException e) {
+            // TODO: or did an exception occur?
+        }
+    }
+}
