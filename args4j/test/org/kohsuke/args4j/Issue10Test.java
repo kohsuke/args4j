9417e47b5716641d16a58f124f4fb4700c570e83
jhm <null>
2008-06-19 12:50:03 +0000

    Issue10 fixed

--- args4j/test/org/kohsuke/args4j/Issue10Test.java
+++ args4j/test/org/kohsuke/args4j/Issue10Test.java
@@ -17,34 +17,13 @@ public class Issue10Test extends TestCase {
 		BECAUSE, OF, ITS, HUGE, LIST, Of, VALUES,
 		SO, IT, WILL, CRASH
 	}
-	
+
+	// The bug should be fixed with changing from manual printing to printf.
 	public void testIssue10() {
 		CmdLineParser parser = new CmdLineParser(this);
 		parser.printUsage(new ByteArrayOutputStream());
-		// StringIndexOutOfBoundsException with index < 0
-		/*
-		 * CmdLineParser.printOption() calculates the widht of the four colums:
-		 * - 1st column: just one indenting space - length=1
-		 * - 3rd column: just separating " : " - length=3
-		 * - 2nd column: length of the 'name' and 'metadata' of the option
-		 * - 4th column: rest : usageWidht (80) minus length of the other three columns
-		 * In this case 
-		 * - 'name' is "-enum"
-		 * - 'metadata' is the list of all Enum-values
-		 * So this list is bigger than 76 (80-1-3) and therefore the width of the 4th columns 
-		 * gets negative - which isn't the right think ;-)
-		 * 
-		 * a)Tweaking: just set the 4th length to 0 if it was positive:
-		 * + fast to implement
-		 * - long metadata break the maximum columns width
-		 * - usage information is hidden because of columnwidth=0
-		 * 
-		 * b)Changing of the output layout using line wrappers
-		 * Line wrapping should be done in nameAndMetadata AND in usage (columns 2+4).
-		 * 
-		 * BTW: maybe the output could be made more easily with the Java5 printf method ;-)
-		 * 
-		 */
-	}
+		// occurred error: StringIndexOutOfBoundsException with index < 0
 
+	}
+	
 }
