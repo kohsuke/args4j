9f61ca4e0f224f54d5080f76edac1c542ccdcd0c
kohsuke <null>
2009-01-30 17:00:16 +0000

    doing this differently

--- release.sh
+++ release.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -xe
 cd args4j
-(java -fullversion | grep 1.5) || exit 1
-
 maven clean:clean javanet:dist javanet:site javanet:deploy-jar jar:install
 cd ../args4j-tools
 maven javanet:dist javanet:deploy-jar jar:install
