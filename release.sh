9cc19e6d34a87b44c7b095d38f0ddd849050e817
kohsuke <null>
2009-01-30 17:18:06 +0000

    now I remember how to do a release here

--- release.sh
+++ release.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -xe
+# run this after manually updating project.xml to point to the next release version
+mvn -B release:prepare release:perform
 cd args4j
-maven clean:clean javanet:dist javanet:site javanet:deploy-jar jar:install
+maven clean:clean javanet:dist javanet:site jar:install
 cd ../args4j-tools
-maven clean:clean javanet:dist javanet:deploy-jar jar:install
+maven clean:clean javanet:dist jar:install
 cd ..
 maven javanet:site
