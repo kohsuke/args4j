15702531559af18d0bdef2f42c0a1b42e076bfbc
kohsuke <null>
2009-01-30 16:55:26 +0000

    make sure we run with Java5

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -xe
 cd args4j
+(java -fullversion | grep 1.5) || exit 1
+
 maven javanet:dist javanet:site javanet:deploy-jar jar:install
 cd ../args4j-tools
 maven javanet:dist javanet:deploy-jar jar:install
