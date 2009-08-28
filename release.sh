4ddab1204e505d9ced575d6af6d943a264831818
jhm <null>
2009-08-28 08:42:27 +0000

    Hint for updating changes.xml

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -xe
-# run this after manually updating project.xml to point to the next release version
+# run this after manually updating project.xml and xdocs/changes.xml 
+# to point to the next release version
 mvn -B release:prepare release:perform
 cd args4j
 maven clean:clean javanet:dist javanet:site jar:install
