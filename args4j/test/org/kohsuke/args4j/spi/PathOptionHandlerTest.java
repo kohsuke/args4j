346c8b82ef999b814bdd6b92e99cfcd6fb5fd805
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-07-01 10:36:59 -0700

    TAB -> WS

--- args4j/test/org/kohsuke/args4j/spi/PathOptionHandlerTest.java
+++ args4j/test/org/kohsuke/args4j/spi/PathOptionHandlerTest.java
@@ -19,36 +19,35 @@ public class PathOptionHandlerTest extends TestCase {
 
     private PathOptionHandler handler;
 
-    	@Override
-    	public void setUp() {
-    		handler = new PathOptionHandler(null, null, null);
-    	}
+    @Override
+    public void setUp() {
+        handler = new PathOptionHandler(null, null, null);
+    }
 
-    	public void testParseSuccess() throws Exception {
-    		Path expectedIp = Paths.get(TEST_1);
-    		Path ip = handler.parse(TEST_1);
+    public void testParseSuccess() throws Exception {
+        Path expectedIp = Paths.get(TEST_1);
+        Path ip = handler.parse(TEST_1);
 
-    		assertEquals(expectedIp, ip);
-    	}
+        assertEquals(expectedIp, ip);
+    }
 
-    	public void testParseFailure() throws Exception {
-    		try {
-    		    handler.parse(TEST_2);
-    		}
-            catch (CmdLineException e) {
-    			//assertEquals("\"a.b.c.d\" must be an IP address", e.getMessage());
-    			return;
-    		}
-    		fail("Invalid Path Should Have Thrown Exception");
-    	}
-    	public void testNullParseFailure() throws Exception {
-    		try {
-    		    handler.parse(null);
-    		}
-            catch (CmdLineException e) {
-    			//assertEquals("\"a.b.c.d\" must be an IP address", e.getMessage());
-    			return;
-    		}
-    		fail("Null Path Should Have Thrown Exception");
-    	}
+    public void testParseFailure() throws Exception {
+        try {
+            handler.parse(TEST_2);
+        } catch (CmdLineException e) {
+            //assertEquals("\"a.b.c.d\" must be an IP address", e.getMessage());
+            return;
+        }
+        fail("Invalid Path Should Have Thrown Exception");
+    }
+
+    public void testNullParseFailure() throws Exception {
+        try {
+            handler.parse(null);
+        } catch (CmdLineException e) {
+            //assertEquals("\"a.b.c.d\" must be an IP address", e.getMessage());
+            return;
+        }
+        fail("Null Path Should Have Thrown Exception");
+    }
 }
