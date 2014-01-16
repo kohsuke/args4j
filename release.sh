53a46bdb54f6810efb8b4d757b5077e6aa4c88a4
Emmanuel Bourg <ebourg@apache.org>
2014-01-16 11:42:44 +0100

    Remove the old Maven 1 build

--- release.sh
+++ release.sh
@@ -2,9 +2,3 @@
 # run this after manually updating project.xml and xdocs/changes.xml 
 # to point to the next release version
 mvn -B release:prepare release:perform
-cd args4j
-maven clean:clean javanet:dist javanet:site jar:install
-cd ../args4j-tools
-maven clean:clean javanet:dist jar:install
-cd ..
-maven javanet:site
