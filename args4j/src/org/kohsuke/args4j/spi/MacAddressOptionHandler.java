44e02adb7eb36b646fd9fdd6ab6d15b1212ad990
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2014-02-18 14:31:22 +0100

    Exception during runtime resolved: After calling the parse()-method the underlying OneArgumentOptionHandler calls setter.addValue(value) where value is a byte[6]. This method does - with a detour over ArrayFieldSetter - something like
    byte[] field = new byte[1];
    mac[0] = value;
    
    which causes an IllegalArgumentException.
    
    I have modified my OptionHandler to take the (correct) bypass over setter.asFieldSetter().addValue(value) which solves the problem. Therefore MacAddressOptionHandler is not longer a OneArgumentOptionHandler.

--- args4j/src/org/kohsuke/args4j/spi/MacAddressOptionHandler.java
+++ args4j/src/org/kohsuke/args4j/spi/MacAddressOptionHandler.java
@@ -9,56 +9,59 @@ import org.kohsuke.args4j.OptionDef;
  * 
  * @author Tobias Stolzmann
  */
-public class MacAddressOptionHandler extends OneArgumentOptionHandler<Byte[]> {
+public class MacAddressOptionHandler extends OptionHandler<Byte[]> {
     public MacAddressOptionHandler(CmdLineParser parser, OptionDef option,
 	    Setter<? super Byte[]> setter) {
 	super(parser, option, setter);
     }
 
-    @Override
-    protected Byte[] parse(String argument) throws CmdLineException {
-	String[] macString = null;
+	@Override
+	public int parseArguments(Parameters params) throws CmdLineException {
+		String macString = params.getParameter(0);
+		String[] macStringArray = null;
 
-	if (argument.matches("[0-9a-fA-F]{12}"))
-	    /*
-	     * When entering this clause our MAC address is a hexadecimal string
-	     * with 12 digit. Hence we have no delimiter to split. So we simply
-	     * split after each two characters.
-	     */
-	    macString = argument.split("(?<=\\G.{2})");
-	else if (argument.matches("([0-9a-fA-F]{1,2}[^0-9a-fA-F]+){5}[0-9a-fA-F]{1,2}"))
-	    /*
-	     * When entering this clause our MAC address is a in the form
-	     * XX#XX#XX#XX#XX#XX where XX is a hexadecimal string with one or two
-	     * digits and # is a delimiter which contains no hexadecimal digit.
-	     * In most cases # is a dash (-), a colon (:) or a space ( ).
-	     * We just need to split by our delimiter.
-	     */
-	    macString = argument.split("[^0-9a-fA-F]+");
-	else
-	    throw new CmdLineException(owner,
-		    Messages.ILLEGAL_MAC_ADDRESS.format(argument));
+		if (macString.matches("[0-9a-fA-F]{12}"))
+		    /*
+		     * When entering this clause our MAC address is a hexadecimal string
+		     * with 12 digit. Hence we have no delimiter to split. So we simply
+		     * split after each two characters.
+		     */
+		    macStringArray = macString.split("(?<=\\G.{2})");
+		else if (macString.matches("([0-9a-fA-F]{1,2}[^0-9a-fA-F]+){5}[0-9a-fA-F]{1,2}"))
+		    /*
+		     * When entering this clause our MAC address is a in the form
+		     * XX#XX#XX#XX#XX#XX where XX is a hexadecimal string with one or two
+		     * digits and # is a delimiter which contains no hexadecimal digit.
+		     * In most cases # is a dash (-), a colon (:) or a space ( ).
+		     * We just need to split by our delimiter.
+		     */
+		    macStringArray = macString.split("[^0-9a-fA-F]+");
+		else
+		    throw new CmdLineException(owner,
+			    Messages.ILLEGAL_MAC_ADDRESS.format(macString));
 
-	Byte[] mac = new Byte[6];
-	for (int i = 0; i < 6; i++)
-	    /*
-	     * Yes, we really need to parse a short here... ;-)
-	     * Explanation: All six MAC address parts are unsigned bytes in
-	     * hexadecimal representation. They lay between 0x00 and 0xff
-	     * respectively 0 and 255. The Java data type byte is signed. It
-	     * lays between -128 and 127. Therefore we need to "convert" the
-	     * upper half of our unsigned values to negative to obtain the
-	     * correct bit representation (think of the two's complement). This
-	     * is done best by parsing short (or int or long) and casting to
-	     * byte.
-	     */
-	    mac[i] = (byte) Short.parseShort(macString[i], 16);
+		Byte[] mac = new Byte[6];
+		for (int i = 0; i < 6; i++)
+		    /*
+		     * Yes, we really need to parse a short here... ;-)
+		     * Explanation: All six MAC address parts are unsigned bytes in
+		     * hexadecimal representation. They lay between 0x00 and 0xff
+		     * respectively 0 and 255. The Java data type byte is signed. It
+		     * lays between -128 and 127. Therefore we need to "convert" the
+		     * upper half of our unsigned values to negative to obtain the
+		     * correct bit representation (think of the two's complement). This
+		     * is done best by parsing short (or int or long) and casting to
+		     * byte.
+		     */
+		    mac[i] = (byte) Short.parseShort(macStringArray[i], 16);
 
-	return mac;
-    }
+		setter.asFieldSetter().addValue(mac);		
+		return 1;
+	}
 
     @Override
     public String getDefaultMetaVariable() {
 	return "<mac address>";
     }
+    
 }
