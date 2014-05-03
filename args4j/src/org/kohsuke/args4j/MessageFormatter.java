77d4dd529a02145f19bb81ca5dbadf03ce9b563e
Stephan Fuhrmann <s_fuhrm@gmx.de>
2014-05-03 16:24:48 +0200

    Experimental localization plus in the CmdLineException

---
+++ args4j/src/org/kohsuke/args4j/MessageFormatter.java
@@ -0,0 +1,34 @@
+package org.kohsuke.args4j;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+enum Messages {
+    MISSING_OPERAND,
+    UNDEFINED_OPTION,
+    NO_ARGUMENT_ALLOWED,
+    REQUIRED_OPTION_MISSING,
+    TOO_MANY_ARGUMENTS,
+    REQUIRED_ARGUMENT_MISSING,
+    METADATA_ERROR,
+    MULTIPLE_USE_OF_ARGUMENT,
+    MULTIPLE_USE_OF_OPTION,
+    UNKNOWN_HANDLER,
+    NO_OPTIONHANDLER,
+    NO_CONSTRUCTOR_ON_HANDLER,
+    REQUIRES_OPTION_MISSING
+    ;
+
+    private static ResourceBundle rb;
+
+    public String format( Object... args ) {
+        synchronized(Messages.class) {
+            if(rb==null)
+                rb = ResourceBundle.getBundle(Messages.class.getName());
+            return MessageFormat.format(rb.getString(name()),args);
+        }
+    }
+}
