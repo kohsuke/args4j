ff847b3f4205f7d9c794a0f62f5638c0866a59a8
Erb <Erb@CooperDooper.org>
2014-06-05 22:44:42 -0400

    test new way of setting width

--- args4j/test/org/kohsuke/args4j/LongUsageTest.java
+++ args4j/test/org/kohsuke/args4j/LongUsageTest.java
@@ -22,5 +22,23 @@ public class LongUsageTest extends Args4JTestBase<LongUsage> {
             assertEquals("Second line wrong", expectedLine2, usageLines[1]);
         }
     }
-   
+
+    public void testUsageMessageWithNewWayToSet() {
+        args = new String[]{"-wrong-usage"};
+        // set Widescreen otherwise a line wrapping must occur
+        parser = new CmdLineParser(testObject,
+                ParserProperties.defaults().withUsageWidth(120));
+        try {
+        	// start parsing
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            String expectedLine1 = " -LongNamedStringOption USE_A_NICE_STRING : set a string";
+            String expectedLine2 = " -i N                                     : set an int";
+            String[] usageLines = getUsageMessage();
+            assertUsageLength(2);
+            assertEquals("First line wrong", expectedLine1, usageLines[0]);
+            assertEquals("Second line wrong", expectedLine2, usageLines[1]);
+        }
+    }
+
 }
